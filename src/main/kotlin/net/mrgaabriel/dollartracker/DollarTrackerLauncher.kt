6b8ebce49e73374c8c3db38ca0f85a26b40ff70b
MrGaabriel <hello@mrgaabriel.space>
2019-01-15 16:22:48 -0200

    Revert "Arredondar o valor para duas casas decimais"
    This reverts commit 8c4144d400441a88dcd6c055e751182f77c5ed80.

--- src/main/kotlin/net/mrgaabriel/dollartracker/DollarTrackerLauncher.kt
+++ src/main/kotlin/net/mrgaabriel/dollartracker/DollarTrackerLauncher.kt
@@ -14,7 +14,6 @@ import net.dv8tion.jda.api.entities.Activity
 import org.slf4j.LoggerFactory
 import java.awt.Color
 import java.io.File
-import java.text.DecimalFormat
 import java.time.OffsetDateTime
 import kotlin.concurrent.thread
 
@@ -90,14 +89,7 @@ object DollarTrackerLauncher {
 
         val results = payload["results"].obj
         val usdbrl = results["USD_BRL"].obj
-
-        // Gambiarra
-        val splittedValue = (usdbrl["val"].double).toString().split(".")
-
-        val integers = splittedValue[0]
-        val cents = splittedValue[1].substring(0..1)
-
-        val value = "${integers}.${cents}".toDouble()
+        val value = usdbrl["val"].double
 
         logger.info("Preço atual do dólar: ${value} BRL")
 
